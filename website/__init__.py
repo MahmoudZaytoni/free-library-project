e401e7b5e41a2a4aca244069184003ff2a55c6f9
MahmoudZaytoni <mahmoudzaytoni2002@gmail.com>
2023-02-10 21:31:31 +0200

    Create "SQLAlchemy Database"

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
   app = Flask(__name__)
+  app.config['SECRET_KEY'] = "secretKey"
+
+  # Add Database
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
+  with app.app_context():
+    db.create_all()
 
   from .views import views
   from .auth import auth
@@ -9,10 +21,9 @@ def create_app():
   app.register_blueprint(views, url_prefix ='/')
   app.register_blueprint(auth, url_prefix='/')
 
-
   # Create Custom Error Pages
-  # - Invalid Url 
 
+  # - Invalid Url 
   @app.errorhandler(404)
   def page_not_found(e):
     type_error = "404 Error"
